cbac23952e7eb686754f3e7e2855e7586c8f603d
Jaime Andrade <jaimeandrade454@gmail.com>
2017-06-30 11:54:05 -0500

    Initial Commit

---
+++ arc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  arc
+//
+//  Created by Jaime_Andrade on 6/30/17.
+//  Copyright © 2017 Jaime Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
